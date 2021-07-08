b3658559637f5c5f62cfbcc639a626d34b1f19ea
raquelcassia <rcassia_scarvalho@hotmail.com>
2021-07-08 17:51:13 -0300

    Steps Acesso
    Funções em cucumber para testes de acesso

---
+++ cypress/integration/Acesso/steps.js
@@ -0,0 +1,45 @@
+/// <reference types="cypress" />
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+
+//   Eu quero fazer login para ter acesso ao meu cadastro.
+
+Given(/^o usuario acessa o site "([^"]*)"$/, (args1) => {
+	console.log(args1);
+	return true;
+});
+
+And(/^o cliente informa que tem 18 anos ou mais$/, () => {
+	return true;
+});
+
+And(/^insere Email "([^"]*)"$/, (args1) => {
+	console.log(args1);
+	return true;
+});
+
+And(/^insere Senha "([^"]*)"$/, (args1) => {
+	console.log(args1);
+	return true;
+});
+
+When(/^o usuario clica no botao Entrar$/, () => {
+	return true;
+});
+
+//Cliente com mais de 18 anos quer acessar seu cadastro com dados corretos 
+
+Then(/^o usuario terá acesso a página principal da sua conta com sucesso.$/, () => {
+	return true;
+});
+
+//Cliente com mais de 18 anos quer acessar seu cadastro com Email incorreto
+
+Then(/^será retornado uma mensagem de aviso E-mail não encontrado.$/, () => {
+	return true;
+});
+
+//Cliente com mais de 18 anos quer acessar seu cadastro com senha incorreta
+
+Then(/^será retornado uma mensagem de aviso Senha inválida$/, () => {
+	return true;
+});
