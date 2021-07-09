0778eb8969723c5157b64b79cfc07c1bd056fa8b
raquelcassia <rcassia_scarvalho@hotmail.com>
2021-07-08 23:19:40 -0300

    Att Steps Acesso com Variáveis de Ambiente
    Configurado arquivo de variáveis de ambiente do cypress
    Atualização dos Steps Acesso

--- cypress/integration/Acesso/steps.js
+++ cypress/integration/Acesso/steps.js
@@ -3,43 +3,44 @@ import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
 //   Eu quero fazer login para ter acesso ao meu cadastro.
 
-Given(/^o usuario acessa o site "([^"]*)"$/, (args1) => {
-	console.log(args1);
-	return true;
+Given(/^o usuario acessa o site "([^"]*)"$/, (url) => {
+	cy.visit(url);
 });
 
 And(/^o cliente informa que tem 18 anos ou mais$/, () => {
-	return true;
+	cy.get("#age-gate-button-yes").click();
 });
 
-And(/^insere Email "([^"]*)"$/, (args1) => {
-	console.log(args1);
-	return true;
+And(/^clica no botão Entrar$/, () => {
+	cy.get("#welcome-button-sign-in").click();
 });
 
-And(/^insere Senha "([^"]*)"$/, (args1) => {
-	console.log(args1);
-	return true;
+And(/^insere Email "([^"]*)"$/, (email_env) => {
+	cy.get("#login-mail-input-email").type(Cypress.env(email_env));
+});
+
+And(/^insere Senha "([^"]*)"$/, (senha_env) => {
+	cy.get("#login-mail-input-password").type(Cypress.env(senha_env));
 });
 
 When(/^o usuario clica no botao Entrar$/, () => {
-	return true;
+	cy.get("#login-mail-button-sign-in").click();
 });
 
 //Cliente com mais de 18 anos quer acessar seu cadastro com dados corretos 
 
 Then(/^o usuario terá acesso a página principal da sua conta com sucesso$/, () => {
-	return true;
+	cy.get("#global-message-Sucesso", {timeout:30000}).should("contain","Sucesso");
 });
 
 //Cliente com mais de 18 anos quer acessar seu cadastro com Email incorreto
 
 Then(/^será retornado uma mensagem de aviso E-mail não encontrado$/, () => {
-	return true;
+	cy.contains("E-mail não encontrado.", {timeout:30000}).should("contain","E-mail não encontrado.");
 });
 
 //Cliente com mais de 18 anos quer acessar seu cadastro com senha incorreta
 
 Then(/^será retornado uma mensagem de aviso Senha inválida$/, () => {
-	return true;
+	cy.contains("Senha inválida", {timeout:30000}).should("contain","Senha inválida");
 });
