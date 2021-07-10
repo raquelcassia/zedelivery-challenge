f5e5a9072bd16c14ffd9afae53154840b86d2cb0
raquelcassia <rcassia_scarvalho@hotmail.com>
2021-07-10 19:01:46 -0300

    Casos de Teste Atualizados e Steps - Pagina de Checkout
    Cliente quer confirmar o endereço Estrada de Jacarepaguá, 7655
    Cliente quer inserir uma instrução para o distribuidor 
    Cliente quer verificar o valor total da compra e seguir para a página de método de pagamento

--- cypress/integration/Checkout.feature
+++ cypress/integration/Checkout.feature
@@ -6,14 +6,6 @@ Background: Cliente acessa pagina checkout
 
     Given que o cliente esteja na página de checkout "https://www.ze.delivery/checkout"
 
-Scenario: Cliente quer verificar o valor total da compra e seguir para a página de metodo de pagamento
-
-    Given que o cliente tenha unidades do produto "Beats Senses 313ml na sacola"
-    When ele multiplicar o "valor unitario" com a "quantidade de unidades"
-    And somar o "valor das quantidades" com o "frete"
-    Then ele terá o valor total da compra
-    And poderá seguir para selecionar a forma de pagamento
-
 Scenario: Cliente quer confirmar o endereço Estrada de Jacarepaguá, 7655
 
     Given que o cliente queira confirmar o endereco apresentado na tela
@@ -21,8 +13,17 @@ Scenario: Cliente quer confirmar o endereço Estrada de Jacarepaguá, 7655
 
 Scenario: Cliente quer inserir uma instrução para o distribuidor 
 
-    Given que o cliente queira adicionar uma isntrução paa o distribuidor
+    Given que o cliente queira adicionar uma instrução paa o distribuidor
     When o pop for apresentado será inserido a instrucao "Olá, por favor tocar o interfone :)"
     And clicar no botao confirmar 
-    Then a instrucao estará gravada
+    Then a instrucao "Olá, por favor tocar o interfone :)" estará gravada
 
+Scenario: Cliente quer verificar o valor total da compra e seguir para a página de metodo de pagamento
+
+    Given que o cliente queira realizar a compra do produto "Beats Senses 313ml"
+    And quer verificar se no checkout consta 6 unidades
+    And sao no valor de 6,49
+    And o subtotal ficará no valor de 38,94
+    And terá frete de 3,99
+    When o valor final da compra de 42,93 for apresentado na tela
+    Then o cliente poderá seguir para selecionar a forma de pagamento
--- cypress/integration/Checkout/checkout.js
+++ cypress/integration/Checkout/checkout.js
@@ -7,28 +7,71 @@ Given(/^que o cliente esteja na página de checkout "([^"]*)"$/, (args1) => {
     cy.pag_checkout(args1);
 });
 
+
+// Cliente quer confirmar o endereço Estrada de Jacarepaguá, 7655
+
+
+Given(/^que o cliente queira confirmar o endereco apresentado na tela$/, () => {
+    cy.get("#checkout-card-address-details").should("contain", "Receber agora em");
+});
+
+Then(/^o endereço deverá ser "([^"]*)"$/, (args1) => {
+    cy.get("#checkout-card-address-details").should("contain", args1);
+
+});
+
+
+// Cliente quer inserir uma instrução para o distribuidor 
+
+
+Given(/^que o cliente queira adicionar uma instrução paa o distribuidor$/, () => {
+    cy.get("#checkout-textarea-checkout-comment").click();
+});
+
+When(/^o pop for apresentado será inserido a instrucao "([^"]*)"$/, (args1) => {
+    cy.get("#checkout-textarea-modal-comment").clear();
+    cy.get("#checkout-textarea-modal-comment").type(args1);
+});
+
+When(/^clicar no botao confirmar$/, () => {
+    cy.get("#checkout-textarea-modal-button").click();
+});
+
+Then(/^a instrucao "([^"]*)" estará gravada$/, (args1) => {
+    cy.get("#checkout-textarea-checkout-comment").should("contain", args1);
+});
+
+
 // Cliente quer verificar o valor total da compra e seguir para a página de metodo de pagamento
 
 
-Given(/^que o cliente tenha unidades do produto "([^"]*)"$/, (args1) => {
-    console.log(args1);
-    return true;
+Given(/^que o cliente queira realizar a compra do produto "([^"]*)"$/, (args1) => {
+    cy.get("#product-name").should("contain", args1);
 });
 
-When(/^ele multiplicar o "([^"]*)" com a "([^"]*)"$/, (args1, args2) => {
-    console.log(args1, args2);
-    return true;
+And(/^quer verificar se no checkout consta 6 unidades$/, () => {
+    cy.get("#product-amount").should("contain", "6")
+   
 });
 
-When(/^somar o "([^"]*)" com o "([^"]*)"$/, (args1, args2) => {
-    console.log(args1, args2);
-    return true;
+And(/^sao no valor de 6,49$/, () => {
+    cy.get("#products-summary").should("contain", "R$\u00a06,49")
+    
 });
 
-Then(/^ele terá o valor total da compra$/, () => {
-    return true;
+And(/^o subtotal ficará no valor de 38,94$/, () => {
+    cy.get("#subtotal").should("contain", "R$\u00a038,94")
+    
 });
 
-Then(/^poderá seguir para selecionar a forma de pagamento$/, () => {
-    return true;
+And(/^terá frete de 3,99$/, () => {
+    cy.get("#shipping-price").should("contain", "R$\u00a03,99")
+});
+
+When(/^o valor final da compra de 42,93 for apresentado na tela$/, () => {
+    cy.get("#total-price").should("contain", "R$\u00a042,93")
+});
+
+Then(/^o cliente poderá seguir para selecionar a forma de pagamento$/, () => {
+    cy.get("#finish-order").click();
 });
