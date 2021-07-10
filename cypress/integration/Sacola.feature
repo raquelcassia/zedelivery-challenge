e7f8e7585816e98adb8732e71e8433e22e2071da
raquelcassia <rcassia_scarvalho@hotmail.com>
2021-07-09 22:09:18 -0300

    Casos de Teste e Steps - Sacola
    Cliente adicionou na sacola um produto e não atingiu valor minimo do pedido
    Cliente quer adicionar mais unidades do produto que ultrapasse o valor minimo
    Cliente quer remover um produto da sacola

--- cypress/integration/Sacola.feature
+++ cypress/integration/Sacola.feature
@@ -2,28 +2,28 @@ Feature: Sacola
 
  Eu quero modificar a quantidade e unidade de produtos na minha sacola antes de confirmar a compra
    
-Background: Cliente acessa pagina principal após login
+Background: Cliente deseja ver sacola 
     
     Given que o cliente está no painel de sacola
 
 Scenario: Cliente adicionou na sacola um produto e não atingiu valor minimo do pedido
 
-    Given que cliente adicionou 1 unidade do produto "Beats Senses 313ml - Unidade" na sacola
-    When o cliente adicionar mais 1 unidade do produto "Beats Senses 313ml - Unidade"
+    Given que cliente adicionou 1 unidade do produto na sacola
+    When o cliente adicionar mais 1 unidade do produto 
     Then a mensagem informando valor faltante para completar valor minimo será apresentada
     And o botao Continuar Compra estará desabilitado
 
  Scenario: Cliente quer adicionar mais unidades do produto que ultrapasse o valor minimo
     
-    Given que cliente tem adicionado 2 unidades do produto "Beats Senses 313ml - Unidade" na sacola
-    When o cliente adicionar mais 1 unidade do produto "Beats Senses 313ml - Unidade"
+    Given que cliente tem adicionado 2 unidades do produto na sacola
+    When o cliente adicionar mais 1 unidade do produto 
     Then a mensagem informando valor faltante para completar valor minimo desaparece
     And o botao Continuar Compra é habilitado
 
-Scenario: Cliente quer excluir um produto da sacola
+Scenario: Cliente quer remover um produto da sacola
    
-    Given que o cliente tem adicionado um produto na sacola "Beats Senses 313ml - Unidade"
-    And clique no botao de lixeira do produto "Beats Senses 313ml - Unidade"
+    Given que o cliente tem adicionado um produto na sacola 
+    And clique no botao de lixeira do produto 
     When for apresentado o pop de confirmação de remoção do produto 
     And o cliente clicar no botao OK
     Then o produto será removido da sacola
---
+++ cypress/integration/Sacola/steps.js
@@ -0,0 +1,73 @@
+/// <reference types="cypress" />
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+
+// Cliente deseja ver sacola
+
+Given(/^que o cliente está no painel de sacola$/, () => {
+    cy.login_busca_painel();
+    
+});
+
+
+//Cliente adicionou na sacola um produto e não atingiu valor minimo do pedido
+
+
+Given(/^que cliente adicionou 1 unidade do produto na sacola$/, () => {
+    cy.get("#product-amount", { timeout: 30000 }).should("contain", "01");
+});
+
+When(/^o cliente adicionar mais 1 unidade do produto$/, () => {
+    cy.get("#product-plus-button").click();
+});
+
+Then(/^a mensagem informando valor faltante para completar valor minimo será apresentada$/, () => {
+    cy.get("#progress-bar", { timeout: 30000 }).should("contain", "para o valor mínimo do pedido");
+});
+
+And(/^o botao Continuar Compra estará desabilitado$/, () => {
+    cy.get("#finish-order").should('not.be.visible')
+});
+
+
+// Cliente quer adicionar mais unidades do produto que ultrapasse o valor minimo
+
+
+Given(/^que cliente tem adicionado 2 unidades do produto na sacola$/, () => {
+    cy.get("#product-plus-button").click();
+    cy.get("#product-amount", { timeout: 30000 }).should("contain", "02");
+});
+
+When(/^o cliente adicionar mais 1 unidade do produto$/, () => {
+    cy.get("#product-plus-button").click();
+});
+
+Then(/^a mensagem informando valor faltante para completar valor minimo desaparece$/, () => {
+    cy.get("#progress-bar", { timeout: 50000 }).should('not.be.visible');
+});
+Then(/^o botao Continuar Compra é habilitado$/, () => {
+    cy.get("#finish-order").should('be.visible')
+});
+
+
+// Cliente quer remover um produto da sacola
+
+
+Given(/^que o cliente tem adicionado um produto na sacola$/, () => {
+    cy.get("#product-card").should("be.visible");
+});
+
+And(/^clique no botao de lixeira do produto$/, () => {
+    cy.get("#product-delete-button").click();
+});
+
+When(/^for apresentado o pop de confirmação de remoção do produto$/, () => {
+    cy.contains("Você deseja remover este produto?").should("be.visible");
+});
+
+And(/^o cliente clicar no botao OK$/, () => {
+    cy.get("#primary-modal-button").click();
+});
+
+Then(/^o produto será removido da sacola$/, () => {
+    cy.get("#product-card").should("not.be.visible");
+});
